28178f08e607f490947237a226fdd7bfdca77ba7
Jonathan Liu <jxliu95@gmail.com>
2019-06-04 18:11:09 +0000

    Added form for POST requests

--- express_server.js
+++ express_server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let randomStr = "";
+  while (randomStr.length < 6) {
+    randomStr = Math.random().toString(36).replace('0.', '');
+  }
+  return randomStr.substring(0, 6);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,6 +25,10 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -29,6 +42,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
